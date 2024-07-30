167993351209c68661a42718ea4b80ffa3aa12a2
Mag1sstr <danilkarachev234@mail.ru>
2024-07-31 04:34:05 +0500

    add pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,44 @@
+import { formatDate } from "../../helpers/formatDate";
+import styles from "./styles.module.css";
+
+export default function Pagination({
+  totalPages,
+  currentPage,
+  handleNextPage,
+  handlePreviousPage,
+  handlePageClick,
+}) {
+  return (
+    <div className={styles.pagination}>
+      <button onClick={handlePreviousPage} className={styles.arrow}>
+        {"<"}
+      </button>
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, index) => {
+          return (
+            <button
+              onClick={() => handlePageClick(index + 1)}
+              className={styles.pageNumber}
+              key={index}
+              style={
+                index + 1 == currentPage
+                  ? {
+                      background: "green",
+                      borderRadius: "50%",
+                      color: "#fff",
+                      cursor: "default",
+                    }
+                  : null
+              }
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+      <button onClick={handleNextPage} className={styles.arrow}>
+        {">"}
+      </button>
+    </div>
+  );
+}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,23 +5,41 @@ import { getNews } from "../../api/apiNews";
 import { useState } from "react";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 export default function Main() {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    async function fetchNews() {
-      try {
-        setIsLoading(true);
-        const response = await getNews();
-        setNews(response.news);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPage = 10;
+  const pageSize = 10;
+  async function fetchNews(currentPage) {
+    try {
+      setIsLoading(true);
+      const response = await getNews(currentPage, pageSize);
+      setNews(response.news);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-    fetchNews();
-  }, []);
+  }
+  useEffect(() => {
+    fetchNews(currentPage);
+  }, [currentPage]);
+
+  function handleNextPage() {
+    if (currentPage < totalPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  function handlePreviousPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function handlePageClick(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
   return (
     <main className={styles.main}>
       {news.length > 0 && !isLoading ? (
@@ -29,12 +47,26 @@ export default function Main() {
       ) : (
         <Skeleton type={"banner"} count={1} />
       )}
-
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+        totalPages={totalPage}
+        currentPage={currentPage}
+      />
       {!isLoading ? (
         <NewsList news={news} />
       ) : (
         <Skeleton type={"item"} count={10} />
       )}
+
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+        totalPages={totalPage}
+        currentPage={currentPage}
+      />
     </main>
   );
 }
